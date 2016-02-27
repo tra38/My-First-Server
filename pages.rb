d45a02d1c86363a5825e7a93c8608ce8b369b3a3
tra38 <tra38@nau.edu>
2016-02-27 17:12:12 -0600

    Display headers properly

--- pages.rb
+++ pages.rb
@@ -1,23 +1,23 @@
 class Page
-	attr_reader :page, :code, :bytesize, :resource
+	attr_reader :page, :code, :bytesize, :resource, :additional_headers
 
 	def initialize(args)
 		@page = args[:page]
 		@code = args[:code]
-		@bytesize = @page.bytesize
 		@resource = args[:resource]
+		@additional_headers = args[:additional_headers]
 	end
 
 	def headers
-		<<-EOT
-			HTTP/1.1 #{code}
-			Content Type: text/html
-			Content-Length #{bytesize}
-			Connection: close
-			EOT
+		headers_array = []
+		headers_array << "HTTP/1.1 #{code}"
+		headers_array << "Content Type: text/html"
+		headers_array << additional_headers if additional_headers
+		headers_array << "Connection: close"
+		headers_array.join("\r\n")
 	end
 
 	def to_s
-		"#{headers}\n#{page}\nTime is #{Time.now}"
+		"#{headers}\r\n#{page}\nTime is #{Time.now}"
 	end
 end
