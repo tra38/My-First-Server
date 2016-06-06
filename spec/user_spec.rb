8ebec69e9952cd3c05a0e30087f74a5a30695446
Tariq Ali <tra38@nau.edu>
2016-06-06 09:04:32 -0500

    Implement a UUID to identify users, and create an ability to search for users based on UUIDs

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe "User" do
     expect(user.password).to eq("California") 
   end
 
+  it "allows us to access a user based on its UUID" do
+    matt = USER_TABLE["mattBaker"]
+    matt_id = matt.user_id
+    matt_again = User.find_by_user_id(matt_id)
+    expect(matt).to eq(matt_again)
+  end
 end
--- users.rb
+++ users.rb
@@ -1,18 +1,34 @@
+require 'securerandom'
+
 USER_TABLE = {}
 
 class User
   attr_accessor :visits
   attr_reader :password
+  attr_reader :user_id
 
   def initialize(password)
     @visits = 0
     @password = password
+    @user_id = SecureRandom.uuid
   end
 
   def self.create_user(username, password)
     user = User.new(password)
     USER_TABLE[username] = user
   end
+
+  def self.find_by_user_id(user_id)
+    USER_TABLE.each do |key, value|
+      if value.user_id == user_id
+        return value
+      end
+    end
+    nil
+  end
+  
 end
 
 User.create_user("mattBaker", "California")
+
+# Should I use IDs to keep track of users? Or should I store the username and password directly into the cookie? Do I store the entire user object into the Cookie itself? Serializing the object? Ugh.
